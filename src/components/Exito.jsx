b8eaaef58c63a800589001422ddb4529a83652a6
RamiroPoblete <ramirop@MacBook-Pro-de-RamiroPoblete.local>
2022-04-24 03:14:25 -0300

    Primer Commit

---
+++ src/components/Exito.jsx
@@ -0,0 +1,9 @@
+const Exito = ({texto}) => {
+    return (
+        <div className='text-left font-wide uppercase text-red-600'>
+            <p>{texto}</p>
+        </div>
+    );
+}
+ 
+export default Exito;
---
+++ src/components/Vacio.jsx
@@ -0,0 +1,26 @@
+
+const Vacio = ({child}) => {
+    return (
+        <div className= "bg-green-800 text-white text-center p-3 uppercase font-bold mb-3 rounded-md"
+          > {child}
+          </div>
+    )
+  }
+  
+  export default Vacio
+
+
+
+
+  /* (rafce)
+
+  import React from 'react'
+  
+  const Vacio = () => {
+    return (
+      <div>Vacio</div>
+    )
+  }
+  
+  export default Vacio
+  */
